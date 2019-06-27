fdf34edfe18104efc1044df69b1b9c81552e90fa
wilomaku <wjgarciah@unal.edu.co>
2019-06-26 23:13:53 -0300

    Reduce No resolutions, change measure, put accuracy threshold

--- aux/default_config.py
+++ aux/default_config.py
@@ -5,13 +5,12 @@ This module contains all the configuration of simulation environment
 DIR_BAS = '/winuno/Unicamp-FEEC/CC_clas_project/images_will/dados_pipe/'
 DIR_SAVE = '/home/wilomaku/Documents/reps/CC_seg_clas/saves/'
 
-REG_EX = 15
 SMOOTHNESS = 700
 DEGREE = 5
 FIT_RES = 0.35
 RESOLS_INF = 0.01
 RESOLS_SUP = 0.5
-RESOLS_STEP = 0.01
+RESOLS_STEP = 0.02
 POINTS = 500
 
 FL_GRAPH = False
